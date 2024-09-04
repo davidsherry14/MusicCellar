d555afcc26b85d1355e78acf3f5adc083f175c1e
keirastanley <keiraestanley@gmail.com>
2024-09-04 20:21:43 +0100

    connect basic javascript file

---
+++ daveysMusic.js
@@ -0,0 +1,67 @@
+export const daveysMusic = [
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+  {
+    imgSrc:
+      "https://c8.alamy.com/comp/2B4ADPY/illustration-of-cool-alien-with-white-background-vector-2B4ADPY.jpg",
+    videoSrc: "https://www.youtube.com/watch?v=gJhlyBJHv9I",
+  },
+];
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("JavaScript connected successfully");
+
+// Access the image and store it in a variable
+// Add click event listener to image that calls a function to change the heading's colour
