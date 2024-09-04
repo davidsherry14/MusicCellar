31685333b892f14df26677c765afac542382327c
davidsherry14 <davidsherry1407@gmail.com>
2024-09-04 20:38:09 +0100

    added some javascript

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.log("JavaScript connected successfully");
 
 // Access the image and store it in a variable
 // Add click event listener to image that calls a function to change the heading's colour
+
+document.getElementById("title").style.color = "brown";
