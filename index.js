209f50c9c25ec42e091775eb8c6107682b035a8a
keirastanley <keiraestanley@gmail.com>
2024-09-04 20:43:18 +0100

    add a function that runs when you click the button

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ console.log("JavaScript connected successfully");
 // Add click event listener to image that calls a function to change the heading's colour
 
 document.getElementById("title").style.color = "brown";
+const button = document.getElementsByTagName("button")[0];
+
+// Declare function called onButtonClick
+function onButtonClick() {
+  // When the function is called, this message is printed to the console
+  console.log("you clicked me!!!");
+}
+
+// Assign the function to the button's onclick property
+button.onclick = onButtonClick;
