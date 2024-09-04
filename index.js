d7d1f539ff88819b7bb0af2c807bcbb6a56ed6a4
davidsherry14 <davidsherry1407@gmail.com>
2024-09-04 20:48:19 +0100

    set up the button feature

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 console.log("JavaScript connected successfully");
 
-// Access the image and store it in a variable
-// Add click event listener to image that calls a function to change the heading's colour
-
-document.getElementById("title").style.color = "brown";
+// Get the button, and store it in a variable
 const button = document.getElementsByTagName("button")[0];
+// Assign the function to the button's onclick property
+button.onclick = onButtonClick;
 
 // Declare function called onButtonClick
 function onButtonClick() {
-  // When the function is called, this message is printed to the console
-  console.log("you clicked me!!!");
+    // When the function is called, the colour of the title changes
+    document.getElementById("title").style.color = "brown";
 }
-
-// Assign the function to the button's onclick property
-button.onclick = onButtonClick;
