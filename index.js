cfc942f36d8abbd471fc5d95ffbfdd19d91ea736
davidsherry14 <davidsherry1407@gmail.com>
2024-10-07 12:51:06 +0100

    Updated Task work

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ button.onclick = onButtonClick;
 function onButtonClick() {
     // When the function is called, the colour of the title changes
     document.getElementById("title").style.color = "brown";
+    document.getElementById("buttontext").innerHTML = "Click me again!"
+    const catpic = document.createElement("img")
+    catpic.src = "https://media.self.com/photos/57d8c2d150778cef321a5c63/master/w_1600%2Cc_limit/cute-kitty-rub-my-belly.jpg"
+    document.body.appendChild(catpic)
+    const imagebox = document.getElementsByClassName("imagebox")[0]
+    imagebox.remove();
 }
